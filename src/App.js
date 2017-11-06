4899e8c96950bf2ada1f1e85f446c3b61f8b35c3
Dana Iliescu <dana.iliescu96@gmail.com>
2017-11-06 23:43:41 +0100

    added global css

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { injectGlobal } from 'emotion'
+
+// font-weight 300 is light, 400 is normal, 800 is extra bold
+injectGlobal`
+  @import url(https://fonts.googleapis.com/css?family=Nunito:300,400,800);
+
+  body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Nunito';
+    font-weight: 400;
+    background-color: grey;
+  }
+`
 
 class App extends Component {
   render() {
