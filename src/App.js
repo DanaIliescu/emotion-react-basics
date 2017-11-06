c03534661ee809f645c32ab05bd743d41ab76ec2
Dana Iliescu <dana.iliescu96@gmail.com>
2017-11-07 00:12:30 +0100

    added styled example and react-emotion package

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React, { Component } from 'react'
 import { injectGlobal } from 'emotion'
+import styled from 'react-emotion'
 
 // font-weight 300 is light, 400 is normal, 800 is extra bold
 injectGlobal`
   @import url(https://fonts.googleapis.com/css?family=Nunito:300,400,800);
 
-  body {
+  * {
     margin: 0;
     padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
     font-family: 'Nunito';
     font-weight: 400;
     background-color: grey;
   }
 `
 
+const PageWrapper = styled.div`
+  padding: 50px 100px;
+  background-color: lightgrey;
+`
+
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      greeting: 'Hello',
+      participants: ['Mary', 'Joe', 'Anne', 'Mike']
+    }
+  }
+
   render() {
     return (
-      <div>Hello</div>
+      <PageWrapper>{this.state.greeting}</PageWrapper>
     )
   }
 }
